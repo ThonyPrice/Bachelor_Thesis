a077c5bc1b3c93678bc49319124ca5a115c03d6f
Thony Price <thonyprice@gmail.com>
2018-02-21 11:15:44 +0100

    Conversion of data

--- readData.py
+++ readData.py
@@ -15,3 +15,13 @@ def data():
             i += 1
 
     return dataMartix
+
+def convertData():
+    data = getData()
+    target = data[:, [1]]
+    data = np.delete(data, [1], 1)
+    data = data.astype(float)
+    target[target == 'M'] = 1.
+    target[target == 'B'] = 0.
+    target = np.squeeze(np.asarray(target))
+    return data, target
