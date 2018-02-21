6d29254bcfff94152be549e53054a6468f4a17dc
Niklas Lindqvist <niklasdator@n130-p41.eduroam.kth.se>
2018-02-21 10:37:43 +0100

    readData now has a function data() that returns a np.array of all data points

--- readData.py
+++ readData.py
@@ -1,18 +1,17 @@
 import csv
 import numpy as np
 
-dataReader = csv.reader(open("data.csv", newline=''), delimiter=',', quotechar='|')
-dataMartix = np.zeros(shape=(569,32), dtype=object)
-i = 0
+def data():
+    dataReader = csv.reader(open("data.csv", newline=''), delimiter=',', quotechar='|')
+    dataMartix = np.zeros(shape=(569,32), dtype=object)
+    i = 0
 
+    for row in dataReader:
+        if i == 0:
+            #print(row)
+            i += 1
+        else:
+            dataMartix[i-1] = row
+            i += 1
 
-
-for row in dataReader:
-    if i == 0:
-        print(row)
-        i += 1
-    else:
-        dataMartix[i-1] = row
-        i += 1
-
-return dataMartix
+    return dataMartix
