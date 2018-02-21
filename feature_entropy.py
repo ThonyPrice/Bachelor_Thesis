d053236fb26729d43cbd7ca8b72caf3c4c3081c6
Thony Price <thonyprice@gmail.com>
2018-02-21 11:24:06 +0100

    import data

--- feature_entropy.py
+++ feature_entropy.py
@@ -1,17 +1,6 @@
-from readData import data as getData
+from readData import convertData as getData
 import numpy as np
 import pprint as pp
 
-def convertData():
-    data = getData()
-    target = data[:, [1]]
-    data = np.delete(data, [1], 1)
-    data = data.astype(float)
-    target[target == 'M'] = 1.
-    target[target == 'B'] = 0.
-    target = np.squeeze(np.asarray(target))
-    return data, target
-
-data, target = convertData()    
-pp.pprint(data[0:20])
-pp.pprint(np.shape(target))
+data, target = getData()
+# pp.pprint(data[:3])
