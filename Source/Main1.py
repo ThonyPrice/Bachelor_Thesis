ce2af7598fc96161763e67990602611f006c17d8
Thony Price <thonyprice@gmail.com>
2018-04-17 13:56:30 +0200

    Improvements for SFS and SBS

--- Source/Main1.py
+++ Source/Main1.py
@@ -1,86 +1,38 @@
-import pandas
-import numpy as np
 import DataLoader
+import json
 import matplotlib.pyplot as plt
-from sklearn.pipeline import Pipeline
+import numpy as np
+import pandas
 from sklearn import model_selection
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 from sklearn.feature_selection import mutual_info_classif
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import train_test_split
-from sklearn.feature_selection import RFECV
 from mlxtend.feature_selection import SequentialFeatureSelector as SFS
-import pprint as pp
-import sys
-import json
-
-# TODO: Integrate this function into new structure
-def evaluate_sbs(fs_method):
-    print("Evaluating", fs_method)
-    # evaluate each model in turn
-    rez = {'CART':[], 'SVM':[], 'NB':[], 'ANN':[]}
-    results = []
-    names = []
-    scoring = 'accuracy'
-    features = list(range(1, X.shape[1]))
-    # features = list(range(1, 5))
-    for num_features in features:
-        for name, model in models:
-            sfs1 = SFS(model,
-                   k_features=num_features,
-                   forward=True,
-                   floating=False,
-                   scoring=scoring,
-                   cv=10,
-                   n_jobs = 1)
-            X_new = sfs1.fit_transform(X, Y)
-            kfold = model_selection.KFold(n_splits=10, random_state=seed)
-            cv_results = model_selection.cross_val_score(model, X_new, Y, cv=kfold, scoring=scoring)
-            results.append(cv_results)
-            rez[name].append((num_features, cv_results.mean()))
-            names.append(name)
-            msg = "#features: %f, Model: %s:  %f (%f)" % (num_features, name, cv_results.mean(), cv_results.std())
-            print(msg)
-    # features = list(range(5, X.shape[1]))
-    # for num_features in features:
-    #     for name, model in models:
-    #         kfold = model_selection.KFold(n_splits=10, random_state=seed)
-    #         sbs = SFS(  model, k_features=num_features, forward=True, floating=False,
-    #                     scoring='accuracy', cv=10, n_jobs=-1)
-    #         sbs = sbs.fit(X, Y)
-    #         rez[name].append((num_features, sbs.k_score_))
-    #         msg = "#features: %f, Model: %s:  %f, feature_idx: %s " % (num_features, name, sbs.k_score_, sbs.k_feature_idx_)
-    #         print(msg)
-    plotFilter(rez, fs_method)
-
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 def main():
+
     # --- < Set parameters> ---
     seed = 5
     test_size = 0.25
     path = '../Json/'
+    ann_maxiter = 2000
+    ann_epsilon = 1e-6
     # --- </Set parameters> ---
 
     # --- < Load all data> ---
     DATA = DataLoader.DataLoader()
-    all_Data = [DATA.FNA_gb,
+    all_Data = [DATA.Data_mias,
                 DATA.Cleaned_data,
-                DATA.Data_mias,
+                DATA.FNA_gb,
                 DATA.data_FNA,
                 DATA.GSE58606_data
     ]
-    all_Data_names = [  DATA.FNA_gb_name,
-                        DATA.Cleaned_data_name,
-                        DATA.Data_mias_name,
-                        DATA.data_FNA_name,
-                        DATA.GSE58606_data_name
-    ]
-    # all_Data_names = DATA.list_names
+    all_Data_names = DATA.list_names
     # --- </Load all data > ---
 
     # --- < Prepare classifiers > ---
@@ -88,14 +40,17 @@ def main():
     models.append(('CART', DecisionTreeClassifier()))
     models.append(('SVM', SVC()))
     models.append(('NB', GaussianNB()))
-    models.append(('ANN', MLPClassifier()))
+    models.append(('ANN', MLPClassifier(
+        max_iter = ann_maxiter, epsilon=ann_epsilon))
+    )
     # --- </Prepare classifiers > ---
 
     # --- < Collect feature selection methods > ---
     fs_methods = []
+    fs_methods.append((sfsFunction(), 'sfs'))
+    fs_methods.append((sfsFunction(), 'sbs'))
     fs_methods.append((chi2Function(), 'chi2'))
     fs_methods.append((entropyFunction(), 'entropy'))
-    # TODO: Add SBS and SFS here!
     # --- </Collect feature selection methods > ---
 
     # --- < Run >
@@ -109,8 +64,9 @@ def main():
             results, fname = evaluateMethod(method, method_name, models,
                 Xtr, Xtest, Ytr, Ytest, data_name
             )
-        dumpJson(results, fname, path)
+            dumpJson(results, fname, path)
     # --- </Run >
+    print('---*-*-*--- EOF ---*-*-*---')
 
 def splitData(X, y, test_size, seed):
     return train_test_split(X, y, test_size=test_size, random_state=seed)
@@ -129,16 +85,41 @@ def entropyFunction():
     )
     return f
 
+def sfsFunction():
+    f = lambda x, X, Y, model: SFS(model, k_features=x, forward=True,
+            floating=False, n_jobs = 1)
+    return f
+
+def sbsFunction():
+    f = lambda x, X, Y, model: SFS(model, k_features=x, forward=False,
+            floating=False, n_jobs = 1)
+    return f
+
+def subsetFrom(mask, Xtest):
+    mask = np.nonzero(mask)
+    return Xtest[:,mask[0]]
+
 def evaluateMethod(f, f_name, models, Xtr, Xtest, Ytr, Ytest, data_name):
     print('Evaluating ' + f_name + '...')
     results = {'CART':[], 'SVM':[], 'NB':[], 'ANN':[]}
     scoring = 'accuracy'
-    for num_features in range(1, Xtr.shape[1]):
-        Xtr_subset, X_mask = f(num_features, Xtr, Ytr)
-        X_mask = np.nonzero(X_mask)
-        Xtest_subset = Xtest[:,X_mask[0]]
+    features = Xtr.shape[1]
+    step_sz = 1 if features < 100 else 100
+    for num_features in range(1, features, step_sz):
+        if f_name != 'sbs' and f_name != 'sfs':
+            # Filter methods extract features _before_ model is known
+            Xtr_subset, X_mask = f(num_features, Xtr, Ytr)
+            Xtest_subset = subsetFrom(X_mask, Xtest)
         for name, model in models:
-            model.fit(Xtr_subset, Ytr)
+            if f_name == 'sbs' or f_name == 'sfs':
+                # Wrapper methods extract features _after_ model is known
+                sXs = f(num_features, Xtr, Ytr, model)
+                Xtr_subset = sXs.fit_transform(Xtr, Ytr)
+                Xtest_subset = subsetFrom(sXs.k_feature_idx_, Xtest)
+                parameters = sXs.get_params(False)
+                model.set_params(**parameters['estimator'].get_params())
+            else:
+                model.fit(Xtr_subset, Ytr)
             kfold = model_selection.KFold(n_splits=10)
             cv_results = model_selection.cross_val_score(
                 model, Xtest_subset, Ytest, cv=kfold, scoring=scoring
