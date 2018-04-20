b5feec1d7ec700ed2ae4828f2e6753ac86520722
Niklas Lindqvist <niklasdator@n145-p194.eduroam.kth.se>
2018-04-20 10:07:18 +0200

    added loop for dict

--- Source/tableData.py
+++ Source/tableData.py
@@ -1,35 +1,61 @@
 import json
-import pandas
+import pandas as pd
 import numpy as np
 import DataLoader as DataLoader
 import matplotlib.pyplot as plt
 
 
 
-def table(path, filename):
+def table(path, lista_data_names):
 
-    X = pandas.read_json(path+filename)
+    frames = []
+    for name in list_data_names:
+        for method in ['_Chi2', '_Entropy','_sbs', '_sfs']:
+            print(path+name+method)
 
-    print(X)
+            with open(path+name+method+'.json') as json_data:
+
+                tmp = json.load(json_data)
+
+                for key,value in tmp.items():
+
+                    new_values = []
+
+                    for element in value:
+                        new_values.append(element[1])
+
+
+                    value = max(new_values)
+
+                    tmp[key] = value
+
+            print(tmp)
+
+
+            df = pd.DataFrame.from_dict(tmp)
+
+            print(df)
 
 
 
-    df = pandas.DataFrame(X)
+            frames.append(df)
 
+    frames = pd.concat(frames)
 
+    print(frames)
 
     #use: \input{../tables/Cleaned_data_Chi2.json} in tex
 
 
-    def f1(x):
-
-        return '%1.5f' % x[1]
-
-
-    with open("../tables/" + filename + '.tex','w') as tf:
-        tf.write(df.to_latex(buf=None, columns=None, col_space=None, header=True, index=True, na_rep='NaN',
-        formatters=[f1,f1,f1,f1], float_format=True, sparsify=None, index_names=True, bold_rows=False, column_format='|l|l|l|l|l|',
-        longtable=None, escape=None, encoding=None, decimal='.', multicolumn=None, multicolumn_format=None, multirow=False))
+    # def f1(x):
+    #
+    #     return '%1.5f' % x[1]
+    #
+    #
+    # with open("../tables/" + filename + '.tex','w') as tf:
+    #     tf.write(df.to_latex(buf=None, columns=None, col_space=None, header=True, index=True, na_rep='NaN',
+    #     formatters=[f1,f1,f1,f1], float_format=True, sparsify=None, index_names=True, bold_rows=False, column_format='|l|l|l|l|l|',
+    #     longtable=None, escape=None, encoding=None, decimal='.', multicolumn=None, multicolumn_format=None, multirow=False))
 
 
 
@@ -40,6 +66,4 @@ list_data_names= DATA.list_names
 
 path = "../Json/"
 
-for name in list_data_names:
-    for method in ['_Chi2', '_Entropy']:
-        table(path, name+method+'.json')
+table(path, list_data_names)
